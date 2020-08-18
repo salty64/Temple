f66e7e7ef775f23eb643470bf03886665157cf61
salty64 <cedric.arrabie@univ-pau.fr>
2020-08-18 20:21:05 +0200

    camera switch secret + eclipse door

--- script/Door_eclipse.gd
+++ script/Door_eclipse.gd
@@ -1,13 +1,15 @@
 extends StaticBody
 
 
-var done = false 
+var done = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimationPlayer.play("Animation_door")
+	# $AnimationPlayer.play("Animation_door")
 	$AnimationPlayer.seek(0,true)
 	$AnimationPlayer.stop(true)
+	$AudioStreamPlayer3D.stream.loop=false
 	
 
 
@@ -15,8 +17,20 @@ func _ready():
 #func _process(delta):
 #	pass
 
-func play():
-	if !done :
+func play(state:bool):
+
+	if state and !done:
+
+		$AudioStreamPlayer3D.stop()
 		$AnimationPlayer.play("Animation_door")
+		$AudioStreamPlayer3D.play(0)
 		done=true
+
+	elif !state and done:
+
+		$AudioStreamPlayer3D.stop()
+		$AnimationPlayer.play_backwards("Animation_door")
+		$AudioStreamPlayer3D.play(0)
+		done=false
+
 		
--- script/Pivot_battery.gd
+++ script/Pivot_battery.gd
@@ -1,33 +1,34 @@
 extends StaticBody
 
-var laser_pos= false
+var laser_pos = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Action")
-	$AnimationPlayer.seek(4,true)
+	$AnimationPlayer.seek(4, true)
 	$AnimationPlayer.stop(true)
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+
 func play():
-	if !laser_pos :
+	if ! laser_pos:
 		$AnimationPlayer.play_backwards("Action")
-		$AudioStreamPlayer3D.play(0)
-		laser_pos=true
-	else :
-		$AnimationPlayer.play("Switch")
-		$AudioStreamPlayer3D.play(0)
-		laser_pos=false
+		# $AudioStreamPlayer3D.play(0)
+		laser_pos = true
+	else:
+		$AnimationPlayer.play("Action")
+		# $AudioStreamPlayer3D.play(0)
+		laser_pos = false
 
 
 func set_outline_true():
-	$Inter_levier/Meshoutline.visible=true
-	
-	
+	$Batterry/Meshoutline.visible = true
+
+
 func set_outline_false():
-	$Inter_levier/Meshoutline.visible=false
+	$Batterry/Meshoutline.visible = false
