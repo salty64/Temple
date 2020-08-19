305e2949508cdb34ffc6fd0061268b166dc17568
salty64 <cedric.arrabie@univ-pau.fr>
2020-08-19 14:32:45 +0200

    Energy battery

--- script/Door_eclipse.gd
+++ script/Door_eclipse.gd
@@ -1,36 +1,30 @@
 extends StaticBody
 
-
 var done = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# $AnimationPlayer.play("Animation_door")
-	$AnimationPlayer.seek(0,true)
-	$AnimationPlayer.stop(true)
-	$AudioStreamPlayer3D.stream.loop=false
-	
+	# $AnimationPlayer.seek(0,true)
+	# $AnimationPlayer.stop(true)
+	$AudioStreamPlayer3D.stream.loop = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
-func play(state:bool):
 
-	if state and !done:
-
+func play(state: bool):
+	if state and ! done:
 		$AudioStreamPlayer3D.stop()
 		$AnimationPlayer.play("Animation_door")
 		$AudioStreamPlayer3D.play(0)
-		done=true
+		done = true
 
-	elif !state and done:
-
+	elif ! state and done:
 		$AudioStreamPlayer3D.stop()
 		$AnimationPlayer.play_backwards("Animation_door")
 		$AudioStreamPlayer3D.play(0)
-		done=false
-
-		
+		done = false
--- script/Pivot_90.gd
+++ script/Pivot_90.gd
@@ -1,35 +1,39 @@
 extends StaticBody
 
+var done = false
 
-var done = false 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Circle_90/AnimationPlayer.play("Energy_OFF")
-	$Circle_90/AnimationPlayer.seek(0,true)
+	$Circle_90/AnimationPlayer.seek(0, true)
 	$Circle_90/AnimationPlayer.stop(true)
-	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+
 func play():
-	if !done :
+	if ! done:
 		$Circle_90/AnimationPlayer.play("Energy_Switch")
-		
-		done=true
+		$"../Circuit_2/AnimationPlayer".play("Energy_OFF")
+		$"../Guide_90/AnimationPlayer".play("Energy_Switch")
+
+		done = true
 	else:
 		$Circle_90/AnimationPlayer.play_backwards("Energy_Switch")
-		
-		done=false
+		yield($Circle_90/AnimationPlayer, "animation_finished")
+		$"../Circuit_2/AnimationPlayer".play("Energy_ON")
+		$"../Guide_90/AnimationPlayer".play("Energy")
 
-		
+		done = false
+
 
 func set_outline_true():
-	$Circle_90/Meshoutline.visible=true
-	
-	
+	$Circle_90/Meshoutline.visible = true
+
+
 func set_outline_false():
-	$Circle_90/Meshoutline.visible=false
+	$Circle_90/Meshoutline.visible = false
